771f4c24f7fd7ccf76edc4bd89e4d3171ca7c329
tomosud <tomohisaoda@gmail.com>
2024-03-20 06:57:00 +0900

    add save setting

--- popro_control/command.py
+++ popro_control/command.py
@@ -1,6 +1,24 @@
 import requests
 import dearpygui.dearpygui as dpg
 import dearpygui.demo as demo
+import json
+
+
+setting_dict = {'key1': 'value1', 'key2': 'value2'}
+
+
+def save_settings(setting_dict, file_name='tool_setting.ini'):
+    with open(file_name, 'w') as file:
+        json.dump(setting_dict, file)
+
+def load_settings(file_name='tool_setting.ini'):
+    try:
+        with open(file_name, 'r') as file:
+            return json.load(file)
+    except FileNotFoundError:
+        print(f"File '{file_name}' not found. Returning empty dictionary.")
+        return {}
+
 
 
 # command.py
@@ -13,6 +31,20 @@ def print_this():
 
 def save_callback():
     print("Save Clicked")
+    print_this()
+
+    # 使用例
+    # 保存
+
+    save_settings(setting_dict)
+
+    # 読み込み
+    loaded_settings = load_settings()
+    print(loaded_settings)  # {'key1': 'value1', 'key2': 'value2'}    
+
+def add_ui_test():
+    dpg.add_text("Hello worldBB")
+    dpg.add_button(label="Save", callback=save_callback)
 
 def ui():
 
@@ -25,15 +57,14 @@ def ui():
         dpg.add_button(label="Save", callback=save_callback)
         dpg.add_input_text(label="string")
         dpg.add_slider_float(label="float")
+        add_ui_test()
 
     dpg.show_viewport()
     dpg.start_dearpygui()
     dpg.destroy_context()
 
 
-
-
-def demo():
+def demoui():
     dpg.create_context()
     dpg.create_viewport(title='Custom Title', width=600, height=600)
 
