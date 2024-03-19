0ce6bfd1ddc0850392e469e82379355a0fc258d0
tomosud <tomohisaoda@gmail.com>
2024-03-19 23:13:38 +0900

    Update setup.py
    add

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='popro_control',
-    version='0.11',
+    version='0.12',
     packages=find_packages(),
     install_requires=[
         'requests',  # 依存関係としてrequestsを追加
