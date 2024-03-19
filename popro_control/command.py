2ce8667eb5cccebeb6ced354af7eb11a48a03014
tomosud <tomohisaoda@gmail.com>
2024-03-19 23:55:31 +0900

    add dearpygui test
    add dearpygui test

--- popro_control/command.py
+++ popro_control/command.py
@@ -1,4 +1,7 @@
 import requests
+import dearpygui.dearpygui as dpg
+import dearpygui.demo as demo
+
 
 # command.py
 def print_this():
@@ -6,3 +9,37 @@ def print_this():
     
     r = requests.get('https://www.yahoo.co.jp/')
     print(r.content)
+
+
+def save_callback():
+    print("Save Clicked")
+
+def ui():
+
+    dpg.create_context()
+    dpg.create_viewport()
+    dpg.setup_dearpygui()
+
+    with dpg.window(label="Example Window"):
+        dpg.add_text("Hello world")
+        dpg.add_button(label="Save", callback=save_callback)
+        dpg.add_input_text(label="string")
+        dpg.add_slider_float(label="float")
+
+    dpg.show_viewport()
+    dpg.start_dearpygui()
+    dpg.destroy_context()
+
+
+
+
+def demo():
+    dpg.create_context()
+    dpg.create_viewport(title='Custom Title', width=600, height=600)
+
+    demo.show_demo()
+
+    dpg.setup_dearpygui()
+    dpg.show_viewport()
+    dpg.start_dearpygui()
+    dpg.destroy_context()
--- setup.py
+++ setup.py
@@ -6,5 +6,6 @@ setup(
     packages=find_packages(),
     install_requires=[
         'requests',  # 依存関係としてrequestsを追加
+        'dearpygui==1.11.1',  # dearpyguiのバージョンを1.11.1に指定
     ]
 )
