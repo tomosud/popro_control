815b18503c919d7818f07ea07502590f9ec2f6c6
tomosud <tomohisaoda@gmail.com>
2024-03-23 22:17:25 +0900

    Update command.py
    add

--- popro_control/command.py
+++ popro_control/command.py
@@ -32,6 +32,15 @@ def get_json_data(url):
 def ret_all_media(url=testBaseurl):
     gurl = url + '/gp/gpMediaList'
 
+
+    e = check_url(gurl)
+
+    print ('------',e)
+
+    if e[0] == False:
+        print ('error:',e[1])
+        return
+
     gjeson = get_json_data(gurl)
 
     #dir media
@@ -147,6 +156,37 @@ def ui():
         dpg.add_slider_float(label="float")
         add_ui_test()
 
+        with dpg.menu_bar():
+            dpg.add_menu(label="Menu Options")
+        with dpg.child_window(autosize_x=True, height=95):
+            with dpg.group(horizontal=True):
+                dpg.add_button(label="Header 1", width=75, height=75)
+                dpg.add_button(label="Header 2", width=75, height=75)
+                dpg.add_button(label="Header 3", width=75, height=75)
+                dpg.add_button(label="Header 1", width=75, height=75)
+                dpg.add_button(label="Header 2", width=75, height=75)                                            
+        with dpg.child_window(autosize_x=True, height=175):
+            with dpg.group(horizontal=True, width=0):
+                with dpg.child_window(width=102, height=150):
+                    with dpg.tree_node(label="Nav 1"):
+                        dpg.add_button(label="Button 1")
+                    with dpg.tree_node(label="Nav 2"):
+                        dpg.add_button(label="Button 2")
+                    with dpg.tree_node(label="Nav 3"):
+                        dpg.add_button(label="Button 3")
+                with dpg.child_window(width=300, height=150):
+                    dpg.add_button(label="Button 1")
+                    dpg.add_button(label="Button 2")
+                    dpg.add_button(label="Button 3")
+                with dpg.child_window(width=50, height=150):
+                    dpg.add_button(label="B1", width=25, height=25)
+                    dpg.add_button(label="B2", width=25, height=25)
+                    dpg.add_button(label="B3", width=25, height=25)
+        with dpg.group(horizontal=True):
+            dpg.add_button(label="Footer 1", width=175)
+            dpg.add_text("Footer 2")
+            dpg.add_button(label="Footer 3", width=175)
+
     dpg.show_viewport()
     dpg.start_dearpygui()
     dpg.destroy_context()
@@ -186,4 +226,8 @@ for o in data.keys():
     print(o,data[o])
 '''
 
-ret_all_media()
+#ret_all_media()
+
+#ui()
+
+demoui()
