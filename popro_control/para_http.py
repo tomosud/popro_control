7ee19cbdbbe11e7535799872f586ac93be00ddc5
tomosud <tomohisaoda@gmail.com>
2024-03-27 21:47:31 +0900

    dl

--- popro_control/para_http.py
+++ popro_control/para_http.py
@@ -1,54 +1,80 @@
-#非同期で行いたいhttp処理の集約
-
 import asyncio
 import aiohttp
 import time
 import os
 
-async def download_one(url,save_path):
-    start = time.time() #1
-    chunk_size = 10 #2
-    extention = 'zip'
+async def download_one(url, save_path):
+    start = time.time()
+    chunk_size = 10
     filename = url.split('/')[-1]
 
-    async with aiohttp.ClientSession() as session:
-        async with session.get(url, timeout=600) as resp: #3
-            print('start: {}: {}'.format(resp.status,url))
+    print ('------url',url)
+    print ('save_path',save_path)
 
-            #save_path = './{}.{}'.format(filename, extention)
-            
-            with open(save_path, 'wb') as fd:
-                while True:
-                    chunk = await resp.content.read(chunk_size) #4
-                    if not chunk:
-                        break
-                    fd.write(chunk)
+    async with aiohttp.ClientSession() as session:
+        try:
+            async with session.get(url, timeout=600) as resp:
+                # レスポンスステータスコードが200以外の場合にエラーをログに記録
+                if resp.status != 200:
+                    print(f"Error {resp.status} while fetching {url}")
+                    return
+                
+                print(f"Start: {resp.status}: {url}")
+                
+                with open(save_path, 'wb') as fd:
+                    while True:
+                        chunk = await resp.content.read(chunk_size)
+                        if not chunk:
+                            break
+                        fd.write(chunk)
+
+        except Exception as e:
+            # 例外が発生した場合のエラーログ
+            print(f"An error occurred: {e}, while downloading {url}")
+            return
 
     elapsed = round(time.time() - start)
-    print('end: {}: {}s'.format(filename, elapsed))
+    print(f"End: {filename}: {elapsed}s")
 
-def download_many(url_list):
-    loop = asyncio.get_event_loop() #5
-    to_do = [download_one(url) for url in url_list] #6
-    wait_coro = asyncio.wait(to_do) #7
-    res, _ = loop.run_until_complete(wait_coro) #8
-    loop.close()
-    return len(res) #9
-
-
-#{url:save_filename}とappnd savepathを設定する
-
-def download_main_do(url_dict,appnd_savepath):
-
+def download_many(url_dict, appnd_savepath):
+    loop = asyncio.get_event_loop()
     
-    start = time.time() #10
-    count = download_many(url_dict)
+    # コルーチンをタスクに変換
+    to_do = [loop.create_task(download_one(k, appnd_savepath + '/' + v)) for k, v in url_dict.items()]
+
+    wait_coro = asyncio.wait(to_do)
+    res, _ = loop.run_until_complete(wait_coro)
+    loop.close()
+    return len(res)
+
+def download_main(url_dict, appnd_savepath):
+    start = time.time()
+    count = download_many(url_dict, appnd_savepath)
     elapsed = time.time() - start
     msg = '\n{} files downloaded in {:.2f}s'
 
     print(msg.format(count, elapsed))
 
+def download_make_dict_and_do(urls=[],folder_path='C:/temp/DL'):
 
-    
+    if not os.path.exists(folder_path):
+        os.makedirs(folder_path)
 
+    url_dict = {}
 
+    for o in urls:
+        
+        file_name = o.split('/')[-1]
+        #save_path = os.path.join(folder_path,file_name).replace('\\','/')
+        save_path = file_name.replace('\\','/')
+
+        print ('save_path',save_path)
+
+        url_dict[o] = save_path
+
+    download_main(url_dict, folder_path)
+
+# 使用例
+# url_dict = {'http://example.com/file1.zip': 'file1.zip', ...}
+# appnd_savepath = '/path/to/save/directory/'
+# download_main_do(url_dict, appnd_savepath)
--- popro_control/test.py
+++ popro_control/test.py
@@ -4,6 +4,7 @@
 '''
 import command as cm
 import ui as ui
+import para_http
 
 import requests
 from concurrent.futures import ThreadPoolExecutor, as_completed
@@ -13,4 +14,15 @@ from concurrent.futures import ThreadPoolExecutor, as_completed
 
 ui.main()
 
+'''
 
+
+urls =['https://sample-videos.com/video321/mp4/720/big_buck_bunny_720p_20mb.mp4',
+'https://sample-videos.com/video321/mp4/480/big_buck_bunny_480p_20mb.mp4',
+'https://sample-videos.com/video321/mp4/240/big_buck_bunny_240p_20mb.mp4']
+
+para_http.download_make_dict_and_do(urls=urls)
+
+print ('done')
+
+'''
