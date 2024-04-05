4ea9ae383272df9c712b8060fb15b8ae1bbcc6fa
tomosud <tomohisaoda@gmail.com>
2024-04-05 21:19:38 +0900

    module対応
    module対応

--- popro_control/__init__.py
+++ popro_control/__init__.py
@@ -1,1 +1,9 @@
-#
+import os
+import sys
+
+# 現在のファイルのディレクトリ絶対パスを取得
+current_dir = os.path.dirname(os.path.abspath(__file__))
+
+# Pythonの検索パスにこのディレクトリを追加
+if current_dir not in sys.path:
+    sys.path.append(current_dir)
